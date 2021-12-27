0291a5d9175f435314a5779562d1e55999c13b27
yusaristark <yusaristark@gmail.com>
2021-12-27 20:49:50 +0400

    Chapter 2 Challenges

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,6 +1,11 @@
 const val HERO_NAME = "Madrigal"
 
 fun main() {
+    var hasSteed = false
+    val pubsName = "The Unicorn's Horn"
+    val publican = "Michael"
+    var gold = 50
+
     println("The hero announces her presence in the world.")
 
     println(HERO_NAME)
@@ -10,4 +15,6 @@ fun main() {
     println("The hero embarks on her journey to locate the enchanted sword.")
     playerLevel += 1
     println(playerLevel)
+
+    println(HERO_NAME.reversed())
 }
