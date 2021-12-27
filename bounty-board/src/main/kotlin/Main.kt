2dce0f743c7b4a270099a1c9aa814f97c96152a2
yusaristark <yusaristark@gmail.com>
2021-12-27 22:09:11 +0400

    Chapter 3

--- bounty-board/src/main/kotlin/Main.kt
+++ bounty-board/src/main/kotlin/Main.kt
@@ -1,13 +1,36 @@
 const val HERO_NAME = "Madrigal"
 
 fun main() {
-    println("The hero announces her presence in the world.")
+    println("The hero announces her presence to the world.")
 
     println(HERO_NAME)
     var playerLevel = 4
     println(playerLevel)
 
-    println("The hero embarks on her journey to locate the enchanted sword.")
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "paladin"
+    val quest: String = when (playerLevel) {
+        1 -> "Meet Mr. Bubbles in the land of soft things."
+        in 2..5 -> {
+            val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "barbarian")
+            if (canTalkToBarbarians) {
+                "Convince the barbarians to call off their invasion."
+            } else {
+                "Save the town from the barbarian invasions."
+            }
+        }
+        6 -> "Locate the enchanted sword."
+        7 -> "Recover the long-lost artifact of creation."
+        8 -> "Defeat Nogartse, bringer of death and eater of worlds.\""
+        else -> "There are no quests right now."
+    }
+    println("The hero approaches the bounty board. It reads:")
+    println(quest)
+
+    println("Time passes...")
+    println("The hero returns from her quest.")
+
     playerLevel += 1
     println(playerLevel)
 }
