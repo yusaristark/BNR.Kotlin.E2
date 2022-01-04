f5ca5985cd4d6528857ef7b21ca1d54762aa8455
yusaristark <yusaristark@gmail.com>
2022-01-04 16:14:59 +0400

    Chapter 8

---
+++ NyetHack/src/main/kotlin/Narrator.kt
@@ -0,0 +1,52 @@
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+var narrationModifier: (String) -> String = { it }
+
+inline fun narrate(message: String, modifier: (String) -> String = { narrationModifier(it) }) {
+    println(modifier(message))
+}
+
+fun changeNarratorMood() {
+    val mood: String
+    val modifier: (String) -> String
+
+    when (Random.nextInt(1..5)) {
+        1 -> {
+            mood = "loud"
+            modifier = { message ->
+                val numExclamationPoints = 3
+                message.uppercase() + "!".repeat(numExclamationPoints)
+            }
+        }
+        2 -> {
+            mood = "tired"
+            modifier = { message ->
+                message.lowercase().replace(" ", "... ")
+            }
+        }
+        3 -> {
+            mood = "unsure"
+            modifier = { message ->
+                "$message?"
+            }
+        }
+        4 -> {
+            var narrationsGiven = 0
+            mood = "like sending an itemized bill"
+            modifier = { message ->
+                narrationsGiven++
+                "$message.\n(I have narrated $narrationsGiven things)"
+            }
+        }
+        else -> {
+            mood = "professional"
+            modifier = { message ->
+                "$message."
+            }
+        }
+    }
+
+    narrationModifier = modifier
+    narrate("The narrator begins to feel $mood")
+}
