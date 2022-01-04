d15fbcbc97d8473bf1e25e16eb608f23d96d6dee
yusaristark <yusaristark@gmail.com>
2022-01-04 18:53:01 +0400

    Chapter 9 challenges

--- NyetHack/src/main/kotlin/Tavern.kt
+++ NyetHack/src/main/kotlin/Tavern.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.max
 
 private const val TAVERN_MASTER = "Taernyl"
 private const val TAVERN_NAME = "$TAVERN_MASTER's Folly"
@@ -10,9 +11,15 @@ private val menuData = File("data/tavern-menu-data.txt")
     .readText()
     .split("\n")
 
+private data class MenuItem(
+    val type: String,
+    val name: String,
+    val cost: Double
+)
+
 private val menuItems = List(menuData.size) { index ->
-    val (_, name, _) = menuData[index].split(",")
-    name
+    val (type, name, cost) = menuData[index].split(",")
+    MenuItem(type, name, cost.toDouble())
 }
 
 fun visitTavern() {
@@ -29,11 +36,94 @@ fun visitTavern() {
     narrate(patrons.joinToString())
 
     repeat(3) {
-        placeOrder(patrons.random(), menuItems.random())
+        placeOrder(patrons.random(), menuItems.random().name)
     }
+
+    println()
+    printFormattedTavernMenu()
+    println()
+    printAdvancedFormattedTavernMenu()
 }
 
 private fun placeOrder(patronName: String, menuItemName: String) {
     narrate("$patronName speaks with $TAVERN_MASTER to place an order")
     narrate("$TAVERN_MASTER hands $patronName a $menuItemName")
+}
+
+private fun printFormattedTavernMenu() {
+    val welcomeString = "*** Welcome to $TAVERN_MASTER's Folly ***"
+    var maxStringLength = welcomeString.length
+    menuItems.forEach {
+        val stringLength = it.name.length + it.cost.toString().format("%.2d").length
+        maxStringLength = max(stringLength, maxStringLength)
+    }
+
+    if (maxStringLength == welcomeString.length) {
+        println("$welcomeString\n")
+        menuItems.forEach {
+            print(it.name)
+            repeat(maxStringLength - it.name.length - it.cost.toString().format("%.2d").length) {
+                print(".")
+            }
+            println(it.cost)
+        }
+    } else {
+        val newWelcomeString = if (maxStringLength % 2 == 0) {
+            val result: StringBuilder = StringBuilder(welcomeString)
+            repeat((maxStringLength - welcomeString.length) / 2) {
+                result.insert(0, "*")
+                result.append("*")
+            }
+            result
+        } else {
+            val result: StringBuilder = StringBuilder(welcomeString)
+            repeat((maxStringLength - welcomeString.length) / 2 + 1) {
+                result.insert(0, "*")
+                result.append("*")
+            }
+            result
+        }
+        maxStringLength = max(maxStringLength, newWelcomeString.length)
+        println("$newWelcomeString\n")
+        menuItems.forEach {
+            print(it.name)
+            repeat(maxStringLength - it.name.length - it.cost.toString().format("%.2d").length) {
+                print(".")
+            }
+            println(it.cost)
+        }
+    }
+}
+
+private fun printAdvancedFormattedTavernMenu() {
+    val welcomeString = "*** Welcome to $TAVERN_MASTER's Folly ***"
+    var maxStringLength = welcomeString.length
+    menuItems.forEach {
+        val stringLength = it.name.length + it.cost.toString().format("%.2d").length
+        maxStringLength = max(stringLength, maxStringLength)
+    }
+    val sortedList = menuItems.sortedBy {
+        it.type
+    }
+    println("$welcomeString\n")
+    var type = sortedList.first().type
+    repeat(maxStringLength - type.length) {
+        print(" ")
+    }
+    print("~[$type]~\n")
+    sortedList.forEach {
+        if (it.type != type) {
+            type = it.type
+            repeat((maxStringLength - type.length) / 2) {
+                print(" ")
+            }
+            print("~[$type]~\n")
+        }
+        print(it.name)
+        repeat(maxStringLength - it.name.length - it.cost.toString().format("%.2d").length) {
+            print(".")
+        }
+        println(it.cost)
+    }
+
 }
